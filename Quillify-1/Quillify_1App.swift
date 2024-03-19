13f0e4c8f26ec1edafd7724bda5d75f9172401a0
mi11ione <i@mi11ion.io>
2024-03-19 22:57:22 +0300

    Initial Commit

---
+++ Quillify-1/Quillify_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Quillify_1App.swift
+//  Quillify-1
+//
+//  Created by mi11ion on 19/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Quillify_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

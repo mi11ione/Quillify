03b7957564df3c4c9d38f7f4ce4a6ec29a635d05
mi11ione <i@mi11ion.io>
2024-05-26 13:14:15 +0300

    Initial Commit

---
+++ Quillify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quillify
+//
+//  Created by mi11ion on 26/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quillify/QuillifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuillifyApp.swift
+//  Quillify
+//
+//  Created by mi11ion on 26/5/24.
+//
+
+import SwiftUI
+
+@main
+struct QuillifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

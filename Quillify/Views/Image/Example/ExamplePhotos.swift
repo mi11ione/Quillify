bbd22fe784747b897be719d62ac4315f562dbc0a
mi11ione <i@mi11ion.io>
2024-05-27 19:27:47 +0300

    emergency fix

--- Quillify/Views/Image/Example/ExamplePhotos.swift
+++ Quillify/Views/Image/Example/ExamplePhotos.swift
@@ -5,6 +5,5 @@ class ExamplePhotos {
         UIImage(named: "example1.heic"),
         UIImage(named: "example2.jpg"),
         UIImage(named: "example3.jpg"),
-        UIImage(named: "example4.jpg"),
     ].compactMap { $0 }
 }
